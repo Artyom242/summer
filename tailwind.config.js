f676f0c36fbd019e66a0787e25bce2a7043c8304
Artyom242 <artemp876@gmail.com>
2023-03-19 18:20:39 +0700

    without PHP

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  darkMode:'class',
+  theme: {
+    screens:{
+        'xl': {'max':'1200px'},
+        'lg': {'max':'1024px'},
+        'sm': {'max':'620px'}
+    },
+    container:{
+        padding: '15px',
+        center:  true
+        
+    },
+    extend: {
+        colors:{
+            colP: '#515155',
+            colNight: '#232323',
+            bg: '#e9e7e7',
+            footer: '#c4c4c4'
+        }
+    },
+  },
+  plugins: [],
+}
